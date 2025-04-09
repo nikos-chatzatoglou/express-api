7ac3378c6eff14dc5dfa1b968756a2486421a86d
nikos-chatzatoglou <nikoschatzatoglou.dev@gmail.com>
2025-04-09 14:45:47 +0300

    Update Prisma schema to allow nullable passwords for OAuth users and add Google ID field. Refactor app routes to include Google authentication and initialize Passport for user authentication. Introduce Zod schemas for article creation and updates to enforce validation.

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,29 @@
 import express from "express";
+import passport from "passport";
 
+import dotenv from "dotenv";
 import articlesRouter from "./modules/articles/articles.routes";
-import authenticationRouter from "./modules/authentication/authentication.routes";
-import authorizationRouter from "./modules/authorization/authorization.routes";
+import authenticationRoutes from "./modules/authentication/authentication.routes";
+import authorizationRoutes from "./modules/authorization/authorization.routes";
+import googleAuthRouter from "./modules/authentication/googleAuth.routes";
+import "./modules/authentication/passport"; // Import the Passport configuration
+
+dotenv.config(); // Loads variables from .env into process.env
+
 const app = express();
 
 app.use(express.json());
+app.use(passport.initialize()); // Initialize Passport
 
 app.get("/healthcheck", (_req, res) => {
 	res.send("API is up and running!");
 });
 
+app.use("/authentication", authenticationRoutes);
+app.use("/authentication", googleAuthRouter);
+app.use("/authorization", authorizationRoutes);
 app.use("/articles", articlesRouter);
-app.use("/auth", authenticationRouter);
-app.use("/authorization", authorizationRouter);
 
 app.listen(3000, () => {
-	console.log(`App is running!`);
+	console.log("App is running!");
 });
